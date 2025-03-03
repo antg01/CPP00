37a40255ad84e0f51c91cf86eba4d8071b5b1653
agerard <antoine.gerard@intuitim.com>
2025-03-03 13:58:07 +0100

    adds fix space

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: angerard <angerard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 11:44:58 by angerard          #+#    #+#             */
-/*   Updated: 2025/02/06 14:53:29 by angerard         ###   ########.fr       */
+/*   Updated: 2025/03/03 13:57:29 by angerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,23 @@ int main(int ac, char *av[])
         return 0;
     }
 
-    for (int i = 1; i < ac; i++)
+    int i = 1;
+    while (i < ac)
     {
         char *str = av[i];
-        for (int j = 0; str[j] != '\0'; j++)
+        int j = 0;
+        while (str[j] != '\0')
         {
             std::cout << (char)std::toupper(str[j]);
+            j++;
         }
+        if (i < ac - 1)
+        {
+            std::cout << " ";
+        }
+        i++;
     }
-    // adds \n
+
     std::cout << std::endl;
     return 0;
 }
